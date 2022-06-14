a3c69b14201dcc26a69e0b941987505612195af7
Ranvir25 <98032509+Ranvir25@users.noreply.github.com>
2022-06-14 21:27:33 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDT9WLfbtNW4Sw3c6Cf7FFMLztH9RK-GD8",
+    authDomain: "kitter-806c8.firebaseapp.com",
+    databaseURL: "https://kitter-806c8-default-rtdb.firebaseio.com",
+    projectId: "kitter-806c8",
+    storageBucket: "kitter-806c8.appspot.com",
+    messagingSenderId: "292478256854",
+    appId: "1:292478256854:web:0597c7f51bf8f02bb1ab77"}
